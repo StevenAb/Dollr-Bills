e94f214a8eb4e5a6c4af98fc71639341c537f462
StevenAb <shadwclaw@gmail.com>
2017-10-08 01:30:48 -0700

    Added algorithm integration into webapp

--- server.py
+++ server.py
@@ -1,29 +1,27 @@
 import os
 from flask import Flask, request, render_template
+from bill_type import * 
 app = Flask(__name__)
-balance = 0
+
 
 @app.route('/')
 def index():
     return render_template('index.html') 
 
-@app.route('/login', methods=['GET','POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        #login or some shit
-        pass
-    else:
-        #don't do that
-        pass
-
-@app.route('/users/<username>')
-def profile(username=None):
-    return render_template('user.html', name=username)
 
 @app.route('/upload', methods=['GET','POST'])
 def upload():
     print(request.args)
     return render_template('index.html')
     #if request.method == 'POST':
-        
+
+
+@app.route('/results')
+def results():
+    balance = total()
+    print balance
+    return render_template('results.html', balance=balance)
+
+
+def main():
+    index()
